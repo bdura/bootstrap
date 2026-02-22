14aca02a762bc5d9a3b5c393a824da25f9caa732
Basile Dura <basile@bdura.me>
2026-02-22 23:38:49 +0100

    feat: commit flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Bootstrap script";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        bootstrap = pkgs.writeShellApplication {
+          name = "bootstrap";
+          runtimeInputs = with pkgs; [
+            bitwarden-cli
+            jq
+            git
+            nh
+            stow
+          ];
+          text = builtins.readFile ./scripts/bootstrap.sh;
+        };
+      in
+      {
+        apps.${system}.bootstrap = {
+          type = "app";
+          program = "${bootstrap}/bin/bootstrap";
+        };
+      }
+    );
+}
---
+++ scripts/bootstrap.sh
@@ -0,0 +1,45 @@
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+BOLD='\033[1m'
+RESET='\033[0m'
+
+info() { echo -e "${BLUE}${BOLD}==>${RESET} ${BOLD}$*${RESET}"; }
+success() { echo -e "${GREEN}${BOLD}==>${RESET} ${BOLD}$*${RESET}"; }
+warning() { echo -e "${YELLOW}${BOLD}==> Warning:${RESET} $*"; }
+error() { echo -e "${RED}${BOLD}==> Error:${RESET} $*" >&2; }
+dry-run() { echo -e "${YELLOW}${BOLD}==>${RESET} ${BOLD}$*${RESET}"; }
+
+info "Logging in to Bitwarden..."
+BW_SESSION=$(bw login --raw)
+export BW_SESSION
+success "Logged in successfully"
+
+eval "$(ssh-agent)"
+trap 'kill $SSH_AGENT_PID' EXIT
+
+info "Fetching SSH key..."
+bw get item "SSH Key" | jq -r ".notes" | ssh-add -
+success "SSH key loaded into agent"
+
+info "Cloning dotfiles..."
+dry-run "git clone git@github.com:bdura/dotfiles.git ~/.dotfiles"
+success "Dotfiles cloned"
+
+info "Applying stow"
+mkdir .config || true
+cd .dotfiles
+dry-run "stow ."
+cd ~
+success "Dotfiles deployes"
+
+info "Cloning configuration..."
+dry-run "git clone git@github.com:bdura/nix.git ~/machines"
+success "Configuration cloned"
+
+info "Building configuration..."
+cd machines
+dry-run "nh darwin switch --flake . --hostname nix-darwin-btw"
+success "Done! Restart your shell to apply all changes."
